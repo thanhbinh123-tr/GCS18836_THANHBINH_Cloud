beab94e90aab77f1dd80225de2706e8187c1a69c
thanhbinh123-tr <66456717+thanhbinh123-tr@users.noreply.github.com>
2020-11-02 09:58:39 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -8,4 +8,6 @@
                 password=fb6c88dd6722d2caf4064f04e8c02fc2691f49cc960c645993da31033225db5c");
     pg_query("CREATE TABLE testing (id INTEGER)");
     echo "Script Executed!";
+	
+	header("location: ./ATNnew/html/Page");
 ?>
