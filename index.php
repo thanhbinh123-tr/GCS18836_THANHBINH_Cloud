0164d6aff96b377c209ecf00f4e8f36ce8e3b78e
thanhbinh123-tr <66456717+thanhbinh123-tr@users.noreply.github.com>
2020-11-02 10:03:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
     echo "First DB Connection!";
 
-    $db = pg_connect("host=ec2-18-210-90-1.compute-1.amazonaws.com
-                dbname=d6lrq0b5fv0dd6
+    $db = pg_connect("host=ec2-54-157-88-70.compute-1.amazonaws.com
+                dbname=dej4274bafrn9k
                 port=5432
-                user=vvubcmhuaiqdzh
-                password=fb6c88dd6722d2caf4064f04e8c02fc2691f49cc960c645993da31033225db5c");
+                user=nynbfmmylsqjgr
+                password=07c84b2bbcf1d3233c2e4d4c731c0e4f64a6ced68960463f97e366f22d8cc026");
     pg_query("CREATE TABLE testing (id INTEGER)");
     echo "Script Executed!";
 	
